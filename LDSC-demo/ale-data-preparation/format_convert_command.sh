79d116a5fc1c98d262b6160873a6b372e5814efa
yuxiansen <rmyhandsome@163.com>
2023-10-02 06:10:41 +0800

    Create format_convert_command.sh

---
+++ LDSC-demo/ale-data-preparation/format_convert_command.sh
@@ -0,0 +1,7 @@
+./munge_sumstats.py \
+--sumstats /data/asthma_gss_ldsc \
+--out /data/asthma_gss_ldsc_input \
+--merge-alleles /data/w_hm3.snplist
+
+ ./munge_sumstats.py --sumstats /data/ever_smoke_gss_ldsc --out /data/ever_smoke_gss_ldsc_input --merge-alleles /data/w_hm3.snplist
+  ./munge_sumstats.py --sumstats /data/lung_cancer_gss_ldsc --out /data/lung_cancer_gss_ldsc_input --merge-alleles /data/w_hm3.snplist
