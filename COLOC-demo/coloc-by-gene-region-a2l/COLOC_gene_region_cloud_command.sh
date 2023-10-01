315c51db977063c57a31fa38c2a17784cb591429
yuxiansen <rmyhandsome@163.com>
2023-10-02 06:27:09 +0800

    Create COLOC_gene_region_cloud_command.sh

---
+++ COLOC-demo/coloc-by-gene-region-a2l/COLOC_gene_region_cloud_command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#slurm options
+#SBATCH -p intel-fat           #选择多个分区用逗号隔开
+#SBATCH -q hmem                      #Qos只能选一个，否则会报错
+#SBATCH -J shared_variants_detecting                   #作业名称
+#SBATCH -c 20                            #申请8个CPU核心
+#SBATCH --mem=450G
+#SBATCH -o %j.log                       #%j表示实际运行时的作业号
+
+## user's own commands below 
+module load singularity/3.7.1                 #导入软件环境
+singularity exec --bind /storage/yangjianLab/ranmingyu/GRCT/coloc-gg-analysis grct.sif Rscript COLOC_gene_region_cloud.R
